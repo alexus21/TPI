70a958044de382a68335ad644f973d637482f03c
Alex Ulloa <102490533+alexus21@users.noreply.github.com>
2023-08-19 10:14:33 -0600

    Update script.js

--- promisesPractice/script.js
+++ promisesPractice/script.js
@@ -4,16 +4,13 @@ function studentsData() {
     return new Promise(function (resolve, reject){
         setTimeout(function (){
             if(true)
-                resolve("Resuelto exitosamente");
+                resolve(fetch(url));
             reject(new Error("Error al procesar datos"));
         }, 1000);
     });
 }
 
-studentsData().then(function (data){
-    console.log("Datos obtenidos: ", data);
-    return fetch(url);
-})
+studentsData()
     .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error("Error al procesar: ", error));
+    .then(data => console.log("Datos obtenidos: ", data))
+    .catch(error => console.error("Error: ", error));
